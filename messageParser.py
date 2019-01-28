e85135cce437475f973e93e39e79215ed31ca4d5
Sharpnachos <lwalsh7@luc.edu>
2019-01-28 11:41:22 -0600

    Main method in testParser

--- messageParser.py
+++ messageParser.py
@@ -1,25 +1,100 @@
+import re
+import argparse
+
+
 class messageParser(object):
 
     def __init__(self, message):
-        self.message = message
+        if len(message) > 280:
+            raise ValueError("Tweet too long!")
+        else:
+            self.message = message
+            self.topics = {}
+            self.mentions = {}
+            self.links = {}
 
-    def is_mentioned(self, message):
-        pass
+    def extract_topics(self):
+        hashtag_re = re.compile("(?:^|\s)[＃#]{1}(\w+)", re.UNICODE)
+        extracted_topics = hashtag_re.findall(self.message)
+        for i in extracted_topics:
+            topic = i
+            if topic not in self.topics.keys():
+                self.topics.setdefault(topic, 1)
+            else:
+                self.topics[topic] += 1
+
+    def extract_mentions(self):
+        mention_re = re.compile("(?:^|\s)[＠ @]{1}([^\s#<>[\]|{}]+)", re.UNICODE)
+        extracted_mentions = mention_re.findall(self.message)
+        for i in extracted_mentions:
+            mention = i
+            if mention not in self.mentions.keys():
+                self.mentions.setdefault(mention, 1)
+            else:
+                self.mentions[mention] += 1
+
+    def extract_urls(self):
+        url_re = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', re.UNICODE)
+        extracted_urls = url_re.findall(self.message)
+        for i in extracted_urls:
+            url = i
+            if url not in self.links.keys():
+                self.links.setdefault(url, 1)
+            else:
+                self.links[url] += 1
+
+    def is_mentioned(self, username):
+        if username in self.mentions.keys():
+            return True
+        else:
+            return False
 
     def times_mentioned(self, mention):
-        pass
+        return self.mentions.get(mention)
 
     def total_mentions(self):
-        pass
+        return len(self.mentions.keys())
 
     def has_topic(self, topic):
-        pass
+        if topic in self.topics.keys():
+            return True
+        else:
+            return False
 
     def total_topics(self):
-        pass
+        return len(self.topics.keys())
 
     def is_referenced(self, url):
-        pass
+        if url in self.links.keys():
+            return True
+        else:
+            return False
 
     def totalURL(self):
-        pass
+        return len(self.links.keys())
+
+    def processText(self):
+        self.extract_mentions()
+        self.extract_topics()
+        self.extract_urls()
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-m", "--message", help="the tweet to be parsed for mentions, topics, etc.")
+parser.add_argument("-n", "--mentions", help="prints the total mentions", action="store_true")
+parser.add_argument("-t", "--topics", help="prints the total topics", action="store_true")
+parser.add_argument("-l", "--links", help="prints the total number of valid links", action="store_true")
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    tweetParse = messageParser(args.message)
+    tweetParse.processText()
+
+    if args.mentions:
+        print(tweetParse.total_mentions())
+
+    if args.topics:
+        print(tweetParse.total_topics())
+
+    if args.links:
+        print(tweetParse.totalURL())
--- testParser.py
+++ testParser.py
@@ -5,19 +5,38 @@ class TestMessageParserFunctions(unittest.TestCase):
 
     def setUp(self):
         self.parser = messageParser("@franky goes to #hollywood. See http://cnn.com")
+        self.parser.extract_topics()
+        self.parser.extract_mentions()
+        self.parser.extract_urls()
 
     def testMention(self):
         self.assertTrue(self.parser.is_mentioned("franky"))
         self.assertFalse(self.parser.is_mentioned("george"))
-        self.assertEquals(self.parser.times_mentioned("franky"), 1)
-        self.assertEquals(self.parser.times_mentioned("george"), 0)
-        self.assertEquals(self.parser.total_mentions(), 1)
+        self.assertEqual(self.parser.times_mentioned("franky"), 1)
+        self.assertEqual(self.parser.times_mentioned("george"), None)
+        self.assertEqual(self.parser.total_mentions(), 1)
 
     def testTopic(self):
         self.assertTrue(self.parser.has_topic("hollywood"))
         self.assertFalse(self.parser.has_topic("redcarpet"))
-        self.assertEquals(self.parser.total_topics(), 1)
+        self.assertEqual(self.parser.total_topics(), 1)
 
     def testURL(self):
         self.assertTrue(self.parser.is_referenced("http://cnn.com"))
+<<<<<<< HEAD
+        self.assertFalse(self.parser.is_referenced("http://youtube.com"))
         self.assertEquals(self.parser.totalURL(), 1)
+
+    def main():
+        setup()
+        testMention()
+        testTopic()
+        testURL()
+    
+=======
+        self.assertEqual(self.parser.totalURL(), 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
+>>>>>>> b8404e739d3d947c52651f3b07647a1f787dc2f8
